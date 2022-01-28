342d5eb6faccb16affb85c4462f9a9110238da0e
Ezio Santori <Ezio.Santori@wolterskluwer.com>
2022-01-28 14:45:16 +0100

    Add project files.

---
+++ HR.LeaveManagment.Domain/LeaveType.cs
@@ -0,0 +1,13 @@
+﻿using HR.LeaveManagment.Domain.Common;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HR.LeaveManagment.Domain
+{
+  public class LeaveType: BaseDomainEntity
+  {
+
+    public string Name { get; set; }
+    public int DefaultDays { get; set; }
+  }
+}
